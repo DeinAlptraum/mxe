0a3ec5857cf67a23e1dfcb2351f14d2e45c12f1c
Jannick Kremer <51118500+DeinAlptraum@users.noreply.github.com>
2024-07-17 14:15:48 +0200

    Update ffmpeg.mk

--- src/ffmpeg.mk
+++ src/ffmpeg.mk
@@ -50,10 +50,7 @@ define $(PKG)_BUILD
         --extra-ldexeflags="-static-libgcc -static-libstdc++" \
         --enable-libass \
         --extra-ldflags="-fstack-protector -static-libgcc -static-libstdc++" \
-        $($(PKG)_CONFIGURE_OPTS) \
-        CFLAGS="-static-libgcc -static-libstdc++" \
-        CXXFLAGS="-static-libgcc -static-libstdc++" \
-        LDFLAGS="-static-libgcc -static-libstdc++"
-    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' CFLAGS="-static-libgcc -static-libstdc++" CXXFLAGS="-static-libgcc -static-libstdc++" LDFLAGS="-static-libgcc -static-libstdc++"
-    $(MAKE) -C '$(BUILD_DIR)' -j 1 install CFLAGS="-static-libgcc -static-libstdc++" CXXFLAGS="-static-libgcc -static-libstdc++" LDFLAGS="-static-libgcc -static-libstdc++"
+        $($(PKG)_CONFIGURE_OPTS)
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 endef
